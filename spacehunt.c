b43139f922e6a6f4b4deb5cec52a03a14142c325
elementzero23 <elementzero23@gmail.com>
2021-05-13 09:12:19 +0200

    score and game over are working

--- spacehunt.c
+++ spacehunt.c
@@ -8,6 +8,7 @@
 #include "spacehunt_map1.c"
 #include "spacehunt_bg_tiles.c"
 #include "spacehunt_sprite_tiles.c"
+#include "spacehunt_window.c"
 
 #define true 1
 #define false 0
@@ -17,14 +18,27 @@ UBYTE step;
 UWORD seed;
 UBYTE joy;
 
+BOOLEAN game_over;
+// destroy rock = +1, destroy ship = +2
+UINT8 score;
+
 UINT8 shipX, shipY;
 UINT8 shot1X, shot1Y, shot2X, shot2Y;
+UINT8 shotCooldown;
+
 UINT8 opX, opY;
 UINT8 rockX, rockY;
 
 UBYTE rockStep;
 BOOLEAN rockIsMoving;
 BOOLEAN shot1IsMoving, shot2IsMoving;
+
+void performantDelay(UINT8 n) {
+    UINT8 i;
+    for (i = 0; i < n; i++) {
+        wait_vbl_done();
+    }
+}
 
 void play_shot_sound() {
     NR10_REG = 0x1e;
@@ -36,6 +50,7 @@ void play_shot_sound() {
 
 void shoot(UINT8 shotNumber) {
     play_shot_sound();
+    shotCooldown = 0;
     if (shotNumber == 1) {
         shot1X = shipX;
         shot1Y = shipY-4;
@@ -119,13 +134,15 @@ void endShot(UINT8 shotNumber) {
     }
 }
 
-void detect_collission() {
+void detectCollission() {
+    // opponent
     if (shot1X >= opX - 12
         && shot1X <= opX + 10
         && shot1Y <= opY + 8
         && shot1Y >= opY) {
             endShot(1);
             hideOpponent();
+            score += 2;
     }
     if (shot2X >= opX - 12
         && shot2X <= opX + 10
@@ -133,13 +150,16 @@ void detect_collission() {
         && shot2Y >= opY) {
             endShot(2);
             hideOpponent();
+            score += 2;
     }
+    // rock
     if (shot1X >= rockX - 12
         && shot1X <= rockX + 10
         && shot1Y <= rockY + 8
         && shot1Y >= rockY) {
             endShot(1);
             hideRock();
+            score += 1;
     }
     if (shot2X >= rockX - 12
         && shot2X <= rockX + 10
@@ -147,6 +167,60 @@ void detect_collission() {
         && shot2Y >= rockY) {
             endShot(2);
             hideRock();
+            score += 1;
+    }
+    // player ship
+    if (shipX >= rockX - 12
+        && shipX <= rockX + 10
+        && shipY <= rockY + 8
+        && shipY >= rockY
+        || shipX >= opX - 12
+        && shipX <= opX + 10
+        && shipY <= opY + 8
+        && shipY >= opY) {
+            HIDE_SPRITES;
+            puts(" ");
+            puts("game over");
+            printf("score: %d", score);
+            game_over = true;
+        }
+}
+
+void updateScore() {
+    UINT8 tmp = score;
+    UINT8 hundreds = tmp / 100;
+    tmp = score - hundreds*100;
+    UINT8 tens = tmp / 10;
+    tmp = tmp - tens*10;
+    UINT8 map_offset = 13;
+    SpaceHuntWindowMap[17] = (char) (hundreds + map_offset);
+    SpaceHuntWindowMap[18] = (char) (tens + map_offset);
+    SpaceHuntWindowMap[19] = (char) (tmp + map_offset);
+    set_win_tiles(0, 0, 20, 1, SpaceHuntWindowMap);
+}
+
+/** Interrupt handler for LCD
+ *  Once the row indicated by LYC_REG gets drawn, we execute some code.
+ *  Before row 0 gets drawn, we show the window.
+ *  Before line 7 gets drawn, we hide it.
+ */
+void interruptLCD() {
+    switch (LYC_REG)
+    {
+    case 0x00:
+        SHOW_WIN;
+        HIDE_SPRITES;
+        LYC_REG = 0x07;
+        break;
+
+    case 0x07:
+        HIDE_WIN;
+        SHOW_SPRITES;
+        LYC_REG = 0x00;
+        break;
+    
+    default:
+        break;
     }
 }
 
@@ -223,6 +297,19 @@ void main() {
     set_bkg_data(0, 31u, SpaceHuntBGTiles);
     set_bkg_submap(0, 0, 20, SpaceHuntMap1Height, SpaceHuntMap1, SpaceHuntMap1Width);
 
+    // window
+    set_win_data(0, 31u, SpaceHuntBGTiles);
+    set_win_tiles(0, 0, 20, 1, SpaceHuntWindowMap);
+    // prepare the interrupt to only show the top tile row of the window
+    STAT_REG = 0x45;
+    LYC_REG = 0x00;
+    // add the interrupt
+    disable_interrupts();
+    add_LCD(interruptLCD);
+    enable_interrupts();
+    set_interrupts(VBL_IFLAG | LCD_IFLAG);
+
+
     SHOW_BKG;
     SHOW_SPRITES;
 
@@ -235,11 +322,13 @@ void main() {
     shot1IsMoving = false;
     shot2IsMoving = false;
 
+    shotCooldown = 60;
     shot2Y = 0;
 
-    while(1) {
-        scroll_background();
+    score = 0;
+    game_over = false;
 
+    while(1) {
         joy = joypad();
         if (joy & J_LEFT) {
             if (shipX > 12)
@@ -249,68 +338,71 @@ void main() {
                 shipX++;
         }
         UINT8 halfwayUp = 72 - (144-shipY);
+        shotCooldown++;
         if (joy & J_A) {
-            if (!shot1IsMoving && shot2Y <= halfwayUp) {
+            //if (!shot1IsMoving && shot2Y <= halfwayUp) {
+            if (!shot1IsMoving && shotCooldown >= 60) {
                 shoot(1);
             }
-            if (!shot2IsMoving && shot1Y <= halfwayUp) {
+            //if (!shot2IsMoving && shot1Y <= halfwayUp) {
+            if (shot1IsMoving && shotCooldown >= 60) {
                 shoot(2);
             }
         }
 
-        if (joy & J_B) {
-            hideOpponent();
-        }
+        if (!game_over) {
+            scroll_background();
+            // move ship left or right
+            move_sprite(0, shipX, shipY);
+            move_sprite(1, shipX, shipY+8);
+            move_sprite(2, shipX+8, shipY);
+            move_sprite(3, shipX+8, shipY+8);
 
-        // move ship left or right
-        move_sprite(0, shipX, shipY);
-        move_sprite(1, shipX, shipY+8);
-        move_sprite(2, shipX+8, shipY);
-        move_sprite(3, shipX+8, shipY+8);
+            move_shots(); // if they are to be moved
 
-        move_shots(); // if they are to be moved
-
-        // move opponent
-        if (step > 0)
-            opY++;
-        if (opY == 168) {
-            do {
-                opY = 0;
-                opX = arand();
-            } while (opX < 12 || opX > 128);
-            opX = 80;
-        }
-        move_sprite(4, opX, opY);
-        move_sprite(5, opX, opY+8);
-        move_sprite(6, opX+8, opY);
-        move_sprite(7, opX+8, opY+8);
-
-        // move rock
-        if (!rockIsMoving) {
-            if (rockStep == 4) {
-                rockStep = 0;
+            // move opponent
+            if (step > 0)
+                opY++;
+            if (opY == 168) {
                 do {
-                    rockY = 0;
-                    rockX = arand();
-                } while (rockX < 12 || rockX > 128);
-                rockIsMoving = true;
+                    opY = 0;
+                    opX = arand();
+                } while (opX < 12 || opX > 128);
+                opX = 80;
+            }
+            move_sprite(4, opX, opY);
+            move_sprite(5, opX, opY+8);
+            move_sprite(6, opX+8, opY);
+            move_sprite(7, opX+8, opY+8);
+
+            // move rock
+            if (!rockIsMoving) {
+                if (rockStep == 4) {
+                    rockStep = 0;
+                    do {
+                        rockY = 0;
+                        rockX = arand();
+                    } while (rockX < 12 || rockX > 128);
+                    rockIsMoving = true;
+                } else {
+                    rockStep++;
+                }
             } else {
-                rockStep++;
+                move_sprite(8, rockX, rockY);
+                move_sprite(9, rockX, rockY+8);
+                move_sprite(10, rockX+8, rockY);
+                move_sprite(11, rockX+8, rockY+8);
+                rockY++;
+                if (rockY == 168) {
+                    rockIsMoving = false;
+                }
             }
-        } else {
-            move_sprite(8, rockX, rockY);
-            move_sprite(9, rockX, rockY+8);
-            move_sprite(10, rockX+8, rockY);
-            move_sprite(11, rockX+8, rockY+8);
-            rockY++;
-            if (rockY == 168) {
-                rockIsMoving = false;
-            }
+
+            detectCollission();
+
+            updateScore();
         }
 
-        detect_collission();
-
-		// Done processing, yield CPU and wait for start of next frame
-        wait_vbl_done();
+		performantDelay(2);
     }
 }
